f1060bb5de24299097209f4e2184ca5230a4715e
lankeycode <michael.moussaoui@gmail.com>
2025-09-04 23:37:08 +0200

    first commit

---
+++ colors.ts
@@ -0,0 +1,19 @@
+// colors.ts
+
+export const lightColors = {
+    primary: '#F85A3E',
+    secondary: '#FF7733',
+    background: '#E1E6E1',
+    text: '#2C3E50', // Couleur de texte foncée
+    link: '#E15634',
+    inputBorder: '#ccc',
+};
+
+export const darkColors = {
+    primary: '#E15634',
+    secondary: '#FF7733',
+    background: '#2C3E50', // Fond foncé pour le mode sombre
+    text: '#E1E6E1', // Texte clair
+    link: '#FF7733',
+    inputBorder: '#555',
+};
